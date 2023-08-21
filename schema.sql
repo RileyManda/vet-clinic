bbe600ff85d0f7ecb3dad0681a727e3942d46594
RileyManda <rileymanda69@gmail.com>
2023-08-21 19:37:16 +0200

    Moved insert queries to their corerct file

--- schema.sql
+++ schema.sql
@@ -37,36 +37,3 @@ ADD COLUMN species_id integer REFERENCES species(id);
 ALTER TABLE animals
 ADD COLUMN owner_id integer REFERENCES owners(id);
 
--- insert data- owners table
-INSERT INTO owners (full_name, age)
-VALUES ('Sam Smith', 34);
-INSERT INTO owners (full_name, age)
-VALUES ('Jennifer Orwell', 19);
-INSERT INTO owners (full_name, age)
-VALUES ('Bob', 45);
-INSERT INTO owners (full_name, age)
-VALUES ('Melody Pond', 77);
-INSERT INTO owners (full_name, age)
-VALUES ('Dean Winchester', 14);
-INSERT INTO owners (full_name, age)
-VALUES ('Jodie Whittaker', 38);
-
--- insert data species table
-INSERT INTO species (name) VALUES ('Pokemon');
-INSERT INTO species (name) VALUES ('Digimon');
-
-
--- updated animals table with species id
-UPDATE animals
-SET species_id = CASE
-    WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
-    ELSE (SELECT id FROM species WHERE name = 'Pokemon')
-END;
-
---  include owner information
-
-UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith') WHERE name = 'Agumon';
-UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell') WHERE name IN ('Gabumon', 'Pikachu');
-UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob') WHERE name IN ('Devimon', 'Plantmon');
-UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond') WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
-UPDATE animals SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester') WHERE name IN ('Angemon', 'Boarmon');
